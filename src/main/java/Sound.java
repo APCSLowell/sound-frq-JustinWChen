28e9ed982cccb683353537c439f0de825008938d
JustinWChen <143028009+JustinWChen@users.noreply.github.com>
2024-04-16 08:20:10 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -18,11 +18,11 @@ public class Sound
     int count = 0;
     for (int i = 0; i <samples.length;i++){
     if (samples[i]>limit){
-samples[i]==limit;
+samples[i]=limit;
       count++;
     }
       else if (samples[i]<-limit){
-samples[i]==-limit;
+samples[i]=-limit;
         count++;
       }
     }
